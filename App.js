0ae695254e08ba99eb533e83653500db69f2c743
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-03-23 13:42:49 +0100

    create store and apply thunk middleware

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import {View } from 'react-native';
-import AppContainer from './AppContainer';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View } from "react-native";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import reducer from "./reducers/articles";
+import AppContainer from "./AppContainer";
 
 export default function App() {
   return (
-    <View style={{flex: 1}}>
-      <StatusBar style="light" translucent barStyle='light-content'/>
-      <AppContainer />
-    </View>
+    <Provider store={createStore(reducer, applyMiddleware(thunk))}>
+      <View style={{ flex: 1 }}>
+        <StatusBar style="light" translucent barStyle='light-content'/>
+        <AppContainer />
+      </View>
+    </Provider>
   );
 }
-
