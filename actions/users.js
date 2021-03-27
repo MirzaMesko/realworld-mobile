55347b6c27332f06bd059d3581f9bfa7750260e1
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-03-27 21:38:28 +0100

    add register, logout & getCurrentUser functions

--- actions/users.js
+++ actions/users.js
@@ -2,6 +2,9 @@ import AsyncStorage from '@react-native-community/async-storage';
 const axios = require('axios');
 
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const REGISTER_NEW_USER = 'REGISTER_NEW_USER';
+export const LOG_OUT = 'LOG_OUT';
+export const GET_CURRENT_USER_SUCCESS = 'GET_CURRENT_USER_SUCCESS';
 
 export function loginSuccess(token, user) {
     return {
@@ -9,6 +12,29 @@ export function loginSuccess(token, user) {
       token,
       user,
     };
+  }
+
+  export function registrationSuccess(token, user) {
+    return {
+      type: REGISTER_NEW_USER,
+      token,
+      user,
+    };
+  }
+
+  export function logout() {
+    AsyncStorage.removeItem('token');
+    return {
+      type: LOG_OUT,
+    };
+  }
+
+  export function getCurrentUserSuccess(user, token) {
+    return {
+      type: GET_CURRENT_USER_SUCCESS,
+      user,
+      token
+    }
   }
 
 export function login(user) {
@@ -25,3 +51,31 @@ export function login(user) {
         });
   }
 
+  export function registerUser(user) {
+    return (dispatch) =>
+      axios
+        .post('https://conduit.productionready.io/api/users', { user })
+        .then((response) => {
+          AsyncStorage.setItem('token', response.data.user.token);
+          dispatch(registrationSuccess(response.data.user.token, response.data.user));
+          return response
+        })
+        .catch((error) => {
+          return error.response.data.errors
+        });
+  }
+
+  export function getCurrentUser(token) {
+    const url = 'https://conduit.productionready.io/api/user';
+    const headers = { 'Content-Type': 'application/json', 'Authorization' : `Token ${token}` };
+    return (dispatch) => {
+      axios
+      .get(url, {headers})
+      .then((response) => {
+        dispatch(getCurrentUserSuccess(response.data.user, token)) 
+      })
+      .catch((error) => {
+        console.log(error.response.data);
+      });
+    }
+  }
