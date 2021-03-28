11acbf9da63bdc6c0b7ae7a8c1f25e09be68b235
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-03-28 16:37:50 +0200

    add search icon in header to display tags

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,27 +1,42 @@
-import React from 'react';
+import React from "react";
 import { connect } from "react-redux";
-import AsyncStorage from '@react-native-community/async-storage';
-import { View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
-import { getCurrentUser } from '../actions/users';
-import ArticlesList from '../components/ArticlesList';
+import AsyncStorage from "@react-native-community/async-storage";
+import { View, TouchableOpacity, Platform } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { getCurrentUser } from "../actions/users";
+import ArticlesList from "../components/ArticlesList";
 
 function Home(props) {
   React.useEffect(() => {
-    AsyncStorage.getItem('token').then(value => {
+    AsyncStorage.getItem("token").then((value) => {
       props.onGetCurrentUser(value);
     });
-  }, [])
+  }, []);
 
-    return (
-        <View style={{flex: 1}}>
-            <ArticlesList />
-        </View>
-    )
+  return (
+    <View style={{ flex: 1 }}>
+      <ArticlesList />
+    </View>
+  );
 }
 
-  const mapDispatchToProps = dispatch => ({
-    onGetCurrentUser: (token) => dispatch(getCurrentUser(token)),
-  })
+const mapDispatchToProps = (dispatch) => ({
+  onGetCurrentUser: (token) => dispatch(getCurrentUser(token)),
+});
+
+Home["navigationOptions"] = screenProps => ({
+  headerRight: () => (
+    <TouchableOpacity
+      style={{ marginRight: 15 }}
+      onPress={() => screenProps.navigation.navigate('Tags')}
+    >
+      <Ionicons
+        color="white"
+        size={30}
+        name={Platform.OS === "ios" ? "ios-search-outline" : "md-search"}
+      />
+    </TouchableOpacity>
+  ),
+});
 
 export default connect(null, mapDispatchToProps)(Home);
