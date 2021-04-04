4391bebfe9d2a4f96328bc3d3ae6cf6f95a23ffc
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-04-04 19:35:28 +0200

    update component after move to React Navigation v5

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
-import AsyncStorage from "@react-native-community/async-storage";
-import { View, TouchableOpacity, Platform } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { View } from "react-native";
 import { getCurrentUser } from "../actions/users";
 import ArticlesList from "../components/ArticlesList";
 
 function Home(props) {
+
   React.useEffect(() => {
-    AsyncStorage.getItem("token").then((value) => {
-      props.onGetCurrentUser(value);
-    });
+    props.onGetCurrentUser(props.token);
   }, []);
 
   return (
@@ -20,23 +17,12 @@ function Home(props) {
   );
 }
 
+const mapStateToProps = (state) => ({
+  token: state.users.token,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   onGetCurrentUser: (token) => dispatch(getCurrentUser(token)),
 });
 
-Home["navigationOptions"] = screenProps => ({
-  headerRight: () => (
-    <TouchableOpacity
-      style={{ marginRight: 15 }}
-      onPress={() => screenProps.navigation.navigate('Tags')}
-    >
-      <Ionicons
-        color="white"
-        size={30}
-        name={Platform.OS === "ios" ? "ios-search-outline" : "md-search"}
-      />
-    </TouchableOpacity>
-  ),
-});
-
-export default connect(null, mapDispatchToProps)(Home);
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
