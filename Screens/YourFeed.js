006d715ee4d3c0cbb5c86c297b9bad15c6c8fc14
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-04-04 19:27:18 +0200

    move getFeed() request to ArticlesList.js

--- Screens/YourFeed.js
+++ Screens/YourFeed.js
@@ -1,29 +1,14 @@
 import React from 'react';
-import { connect } from "react-redux";
 import { View } from 'react-native';
-import { getFeed } from '../actions/articles';
 import ArticlesList from '../components/ArticlesList';
 
-function YourFeed(props) {
-    const [feedArticles, setFeedArticles] = React.useState([]);
-  React.useEffect(() => {
-    props.onGetFeed(props.token).then((response) => {
-        setFeedArticles(response)
-    })
-  }, [])
+function YourFeed() {
+
     return (
         <View style={{flex: 1}}>
-            <ArticlesList articles={feedArticles} />
+            <ArticlesList feed={true} />
         </View>
     )
 }
 
-  const mapStateToProps = (state) => ({
-    token: state.users.token
-  });
-
-  const mapDispatchToProps = dispatch => ({
-    onGetFeed: (token) => dispatch(getFeed(token)),
-  })
-
-export default connect(mapStateToProps, mapDispatchToProps)(YourFeed);
+export default YourFeed;
