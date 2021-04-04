8b90a29e9b00011f51960a3a2d40205d4c8bdf32
mirzameskovic <pravi.meskovic.mirza@gmail.com>
2021-04-04 14:48:02 +0200

    update React Navigation to v5

--- AppContainer.js
+++ AppContainer.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { Platform } from "react-native";
-import { createSwitchNavigator, createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import { createBottomTabNavigator } from "react-navigation-tabs";
+import { Platform, TouchableOpacity } from "react-native";
+import { connect } from "react-redux";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from "@react-navigation/native"
+import { createStackNavigator } from "@react-navigation/stack"
 import { Ionicons } from "@expo/vector-icons";
 import Home from "./Screens/Home";
 import YourFeed from "./Screens/YourFeed";
@@ -14,170 +15,118 @@ import ArticlesList from "./components/ArticlesList";
 import TagsList from "./components/TagsList";
 import SingleArticle from './components/SingleArticle';
 
-const HomeStack = createStackNavigator({
-  Home: {
-    screen: Home,
-    navigationOptions: {
-      title: "Global feed",
-      headerTintColor: "white",
-      headerStyle: {
-        backgroundColor: "#5cb85c",
-      },
-    },
-  },
-  Tags: {
-    screen: TagsList,
-    navigationOptions: {
-      title: "#tags",
-      headerTintColor: "white",
-      headerStyle: {
-        backgroundColor: "#5cb85c",
-      },
-    },
-  },
-  Articles: {
-    screen: ArticlesList,
-    navigationOptions: {
-      headerTintColor: "white",
-      headerStyle: {
-        backgroundColor: "#5cb85c",
-      },
-    },
-  },
-  SingleArticle: {
-    screen: SingleArticle,
-    navigationOptions: {
-      headerTintColor: "white",
-      headerStyle: {
-        backgroundColor: "#5cb85c",
-      },
-    },
-  },
-});
 
-const YourFeedStack = createStackNavigator({
-  YourFeed: {
-    screen: YourFeed,
-    navigationOptions: {
-      title: "Your feed",
-      headerTintColor: "white",
-      headerStyle: {
-        backgroundColor: "#5cb85c",
-      },
-    },
-  },
-  SingleArticle: {
-    screen: SingleArticle,
-    navigationOptions: {
-      headerTintColor: "white",
-      headerStyle: {
-        backgroundColor: "#5cb85c",
-      },
-    },
-  },
-});
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-const ArticleStack = createStackNavigator({
-  Article: {
-    screen: CreateArticle,
-    navigationOptions: {
-      title: "Create an article",
-      headerTintColor: "white",
-      headerStyle: {
-        backgroundColor: "#5cb85c",
-      },
-    },
-  },
-});
-
-const ProfileStack = createStackNavigator({
-  Profile: {
-    screen: Profile,
-    navigationOptions: {
-      title: "Profile",
-      headerTintColor: "white",
-      headerStyle: {
-        backgroundColor: "#5cb85c",
-      },
-    },
-  },
-  Articles: {
-    screen: ArticlesList,
-    navigationOptions: {
-      headerTintColor: "white",
-      headerStyle: {
-        backgroundColor: "#5cb85c",
-      },
-    },
-  },
-  SingleArticle: {
-    screen: SingleArticle,
-    navigationOptions: {
-      headerTintColor: "white",
-      headerStyle: {
-        backgroundColor: "#5cb85c",
-      },
-    },
-  },
-});
-
-const TabNavigator = createBottomTabNavigator(
-  {
-    Home: HomeStack,
-    YourFeed: YourFeedStack,
-    Article: ArticleStack,
-    Profile: ProfileStack,
-  },
-  {
-    initialRouteName: "Home",
-    defaultNavigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ tintColor }) => {
-        const { routeName } = navigation.state;
-
-        let iconName;
-        if (routeName === "Home") {
-          iconName = `${Platform.OS === "ios" ? "ios-home" : "md-home"}`;
-        } else if (routeName === "Article") {
-          iconName = `${
-            Platform.OS === "ios" ? "add-circle" : "md-add-circle"
-          }`;
-        } else if (routeName === "Profile") {
-          iconName = `${Platform.OS === "ios" ? "ios" : "md"}-person`;
-        } else if (routeName === "YourFeed") {
-          iconName = `${
-            Platform.OS === "ios" ? "ios-bookmark" : "md-bookmark"
-          }`;
-        }
-
-        return <Ionicons name={iconName} size={30} color={tintColor} />;
-      },
-      tabBarOptions: {
-        activeTintColor: Platform.OS === "ios" ? "purple" : "white",
-        style: {
-          height: 56,
-          backgroundColor: Platform.OS === "ios" ? "white" : "#5cb85c",
-          shadowColor: "rgba(0, 0 , 0, 0.24)",
-          shadowOffset: {
-            width: 0,
-            height: 3,
+function HomeScreen() {
+  return (
+    <Tab.Navigator 
+    initialRouteName="Home" 
+    tabBarOptions={{
+      activeTintColor: 'white',
+      inactiveTintColor: '#ccc',
+          style: {
+            height: 56,
+            backgroundColor: Platform.OS === "ios" ? "#5cb85c" : "#5cb85c",
+            shadowColor: "rgba(0, 0 , 0, 0.24)",
+            shadowOffset: {
+              width: 0,
+              height: 3,
+            },
+            shadowradius: 6,
+            shadowOpacity: 1,
           },
-          shadowradius: 6,
-          shadowOpacity: 1,
-        },
-      },
-    }),
-  }
-);
+   }}
+    
+    >
+      <Tab.Screen name="Home" 
+        component={Home}
 
-const SwitchNavigator = createSwitchNavigator(
-  {
-    Main: TabNavigator,
-    Login,
-    Register,
-  },
-  {
-    initialRouteName: "Login",
-  }
-);
+        options={{
+          tabBarIcon: ({color}) => (
+            <Ionicons name={`${Platform.OS === "ios" ? "ios-home" : "md-home"}`} size={30} color={color} />
+          )
+        }}
+        />
+        <Tab.Screen name="Your feed" 
+        component={YourFeed}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Ionicons name={`${Platform.OS === "ios" ? "ios-bookmark" : "md-bookmark"}`} size={30} color={color} />
+          )
+        }}
+        />
+        <Tab.Screen name="My Profile" 
+        component={Profile}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Ionicons name={`${Platform.OS === "ios" ? "ios" : "md"}-person`} size={30} color={color} />
+          )
+        }}
+        />
+        <Tab.Screen name="Add Article" 
+        component={CreateArticle}
+        options={{
+          tabBarIcon: ({color}) => (
+            <Ionicons name={`${Platform.OS === "ios" ? "add-circle" : "md-add-circle"}`} size={30} color={color} />
+          )
+        }}/>
+    </Tab.Navigator>
+  )
+}
 
-export default createAppContainer(SwitchNavigator);
+function AppContainer(props) {
+  return (
+    <NavigationContainer >
+      <Stack.Navigator 
+      initialRouteName='Login'
+      screenOptions={{
+        title: '',
+    headerTintColor: "white",
+    headerStyle: {
+      backgroundColor: "#5cb85c",
+    },
+      }}>
+        { props.isLoggedIn ? 
+        <>
+          <Stack.Screen 
+          name='Home'
+          component={HomeScreen}
+          options={({navigation}) => ({
+            headerRight: () => (
+              <TouchableOpacity
+                style={{ marginRight: 15 }}
+                onPress={() => navigation.navigate('Tags')}
+              >
+                <Ionicons
+                  color="white"
+                  size={30}
+                  name={Platform.OS === "ios" ? "ios-search-outline" : "md-search"}
+                />
+              </TouchableOpacity>
+            )
+          })}
+           />
+           <Stack.Screen name='SingleArticle' component={SingleArticle} />
+           <Stack.Screen name='Tags' component={TagsList} />
+           <Stack.Screen name="Articles" component={ArticlesList} />
+           <Stack.Screen name="Profile" component={Profile} />
+           </>
+      : 
+      <>
+      <Stack.Screen name='Login' component={Login}/>
+      <Stack.Screen name='Register' component={Register}/>
+      </>
+      }
+      
+         
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const mapStateToProps = state => ({
+  isLoggedIn: state.users.isLoggedIn,
+})
+export default connect(mapStateToProps)(AppContainer);
