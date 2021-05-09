5fc4d6414eff576f76a3a63f2c9d7c1987364f03
MirzaMesko <pravi.meskovic.mirza@gmail.com>
2021-05-09 12:15:22 +0200

    add "follow / unfollow article's author" button

--- Screens/Profile.js
+++ Screens/Profile.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
+import AsyncStorage from '@react-native-community/async-storage';
+import { useFocusEffect } from '@react-navigation/native';
 import {
   View,
   TouchableOpacity,
@@ -9,38 +11,61 @@ import {
 } from "react-native";
 import { logout } from "../actions/users";
 import { getArticles } from '../actions/articles';
+import { followUser, unfollowUser, getProfile } from "../actions/users";
 
 function Profile(props) {
+  const [userProfile, setUserProfile] = React.useState([]);
   const { currentUser } = props;
 
-  const onLogout = () => {
-    logout();
-    props.navigation.navigate("Login");
+  const logout = () => {
+    AsyncStorage.removeItem('token');
+    props.onLogout();
   };
 
   const myArticles = () => {
-    props.onGetArticles(`author=${currentUser.username}`).then((response) => {
-      props.navigation.navigate("Articles", {articles: response, title: 'My Articles'});
+    props.onGetArticles(`author=${userProfile.username}`).then((response) => {
+      props.navigation.navigate("Articles", {articles: response, title: `${userProfile.username}'s Articles`});
     });
   }
 
   const favorited = () => {
-    props.onGetArticles(`favorited=${currentUser.username}`).then((response) => {
-      props.navigation.navigate("Articles", {articles: response, title: 'Favorited'});
+    props.onGetArticles(`favorited=${userProfile.username}`).then((response) => {
+      props.navigation.navigate("Articles", {articles: response, title: `Favorited by ${userProfile.username}`});
   })
 }
-  
+
+useFocusEffect(
+  React.useCallback(() => {
+    setUserProfile([]);
+    if(props.route.params) {
+      const { user } = props.route.params;
+      props.onGetProfile(props.token, user).then((response) => {
+        setUserProfile(response.data.profile);
+      });
+    } else {
+      setUserProfile(currentUser)
+    }
+  }, [props])
+)
+
   return (
     <View style={styles.container}>
       <View style={styles.userInfo}>
-        <TouchableOpacity style={{ marginRight: -350, marginTop: -25, marginBottom: 25}}>
+        {userProfile.username === props.currentUser.username ? 
+          <TouchableOpacity 
+          style={{ marginRight: -350, marginTop: -25, marginBottom: 25}}
+          onPress={() => props.navigation.navigate("Settings")}
+          >
           <Text>Edit</Text>
         </TouchableOpacity>
+        : 
+        null
+      }
         <Image
           source={{
             uri:
-              currentUser && currentUser.image !== ""
-                ? currentUser.image
+              userProfile && userProfile.image !== ""
+                ? userProfile.image
                 : "https://reactnative.dev/img/tiny_logo.png",
           }}
           style={{
@@ -51,13 +76,39 @@ function Profile(props) {
             marginBottom: 15
           }}
         />
-        <Text>{currentUser.username}</Text>
-        <Text style={{ color: '#aaa'}}>{currentUser.bio}</Text>
+        <Text>{userProfile.username}</Text>
+        <Text style={{ color: '#aaa'}}>{userProfile.bio}</Text>
+        {userProfile.username === props.currentUser.username ? 
+          null
+        : 
+        <TouchableOpacity
+        onPress={() =>
+          userProfile.following
+            ? unfollowUser(userProfile.username)
+            : followUser(userProfile.username)
+        }
+      >
+        <Text
+          style={{
+            borderWidth: 1,
+            padding: 5,
+            color: "#aaa",
+            margin: 10,
+            maxWidth: "70%",
+            borderColor: "#aaa",
+            borderRadius: 5,
+          }}
+        >
+          {userProfile.following ? "- unfollow" : "+ follow"}{" "}
+          {userProfile.username}
+        </Text>
+      </TouchableOpacity>
+      }
       </View>
         <TouchableOpacity style={styles.options} onPress={() => myArticles()}><Text>My Articles</Text></TouchableOpacity>
         <TouchableOpacity style={styles.options} onPress={() => favorited()}><Text>Favorited</Text></TouchableOpacity>
         <TouchableOpacity style={styles.options}><Text></Text></TouchableOpacity>
-        <TouchableOpacity style={styles.options} onPress={() => onLogout()}><Text>Log out</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.options} onPress={() => logout()}><Text>Log out</Text></TouchableOpacity>
     </View>
   );
 }
@@ -88,13 +139,17 @@ const styles = StyleSheet.create({
 });
 
 const mapStateToProps = (state) => ({
-  user: state.users.username,
   currentUser: state.users.currentUser,
-  articles: state.articles.articles
+  articles: state.articles.articles,
+  token: state.users.token,
 });
 
 const mapDispatchToProps = dispatch => ({
   onGetArticles: (param, offset) => dispatch(getArticles(param, offset)),
+  onFollowUser: (token, username) => dispatch(followUser(token, username)),
+  onUnfollowUser: (token, username) => dispatch(unfollowUser(token, username)),
+  onGetProfile: (token, username) => dispatch(getProfile(token, username)),
+  onLogout: () => dispatch(logout()),
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Profile);
